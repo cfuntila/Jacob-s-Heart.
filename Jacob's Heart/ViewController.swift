1661dcd23b04b507adabbeb4f918b10277e87e03
Moises Funtila <mosesfive@gmail.com>
2018-09-19 13:47:52 -0700

    third commit

--- Jacob's Heart/ViewController.swift
+++ Jacob's Heart/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
@@ -15,7 +16,19 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Rads is the best"
+        content.body = "Rads made me write that"
+        content.sound = UNNotificationSound.default()
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let request = UNNotificationRequest(identifier: "testIdentifier", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //slide Menu
     }
 
     override func didReceiveMemoryWarning() {
